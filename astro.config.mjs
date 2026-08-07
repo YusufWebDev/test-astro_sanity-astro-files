e395363c4182fed55bc4ab74dd85dae9fa5252fa
YusufWebDev <65045006+YusufWebDev@users.noreply.github.com>
2026-08-07 08:43:00 +0200

    initial changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sanity({
+    projectId: "lo5wjfdb",
+    dataset: "test_catalog_dataset",
+    useCdn: false, // for static builds
+  })]
+});
