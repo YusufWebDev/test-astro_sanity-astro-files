e2009cee5a0ede895bc5e4caa205fad3c7f0b0b8
YusufWebDev <65045006+YusufWebDev@users.noreply.github.com>
2026-08-11 22:08:10 +0200

    added styling and initial layout changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import sanity from '@sanity/astro';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [sanity({
     projectId: "lo5wjfdb",
     dataset: "test_catalog_dataset",
     useCdn: false, // for static builds
-  })]
+  })],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
